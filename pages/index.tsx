40ed334cdb427b34e4512741e8001e91e4571344
Samir Prakash <Samir.Prakash@gmail.com>
2022-05-12 22:01:47 -0500

    handle scroll-Y

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Header from '../components/header/header.component'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
