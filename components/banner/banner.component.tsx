769abd9324583794aadd582982520afc3fd5676d
Samir Prakash <Samir.Prakash@gmail.com>
2022-05-17 21:43:25 -0500

    add random image in banner

--- components/banner/banner.component.tsx
+++ components/banner/banner.component.tsx
@@ -18,13 +18,20 @@ function Banner({ netflixOriginals }: Props) {
 
   return (
     <div>
-      <div className="absolute top-0 left-0 h-[95vh] w-screen">
+      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
         <Image
           src={`${baseURL}${movie?.backdrop_path || movie?.poster_path}`}
           layout="fill"
           objectFit="cover"
         />
       </div>
+
+      <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+        {movie?.overview}
+      </p>
     </div>
   )
 }
